03d1d7745a2d8c94d361e2997813a7fdce1eb09a
Jeonghan-Shin <alexsjh23@gmail.com>
2021-03-18 05:19:22 +0900

    [#10] 회원탈퇴 기능 PR merge 중 발생한 conflict 수습

--- src/main/java/com/show/showticketingservice/controller/MyPageController.java
+++ src/main/java/com/show/showticketingservice/controller/MyPageController.java
@@ -1,11 +1,14 @@
 package com.show.showticketingservice.controller;
 
 import com.show.showticketingservice.model.user.UserSession;
+import com.show.showticketingservice.model.user.UserUpdateRequest;
 import com.show.showticketingservice.service.UserService;
 import com.show.showticketingservice.tool.annotation.CurrentUser;
 import com.show.showticketingservice.tool.annotation.UserAuthenticationNecessary;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,8 +20,13 @@ public class MyPageController {
     @PostMapping("/unregister")
     @UserAuthenticationNecessary
     public void deleteUser(@CurrentUser UserSession userSession, @RequestParam String passwordRequest) {
-
         userService.deleteUser(userSession, passwordRequest);
+    }
+
+    @PutMapping
+    @UserAuthenticationNecessary
+    public void updateUserInfo(@CurrentUser UserSession userSession, @RequestBody @Valid UserUpdateRequest userUpdateRequest) {
+        userService.updateUserInfo(userSession, userUpdateRequest);
     }
 
 }
--- src/test/java/com/show/showticketingservice/controller/MyPageControllerTest.java
+++ src/test/java/com/show/showticketingservice/controller/MyPageControllerTest.java
@@ -2,87 +2,184 @@ package com.show.showticketingservice.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.show.showticketingservice.model.enumerations.UserType;
+import com.show.showticketingservice.model.user.UserLoginRequest;
 import com.show.showticketingservice.model.user.UserRequest;
 import com.show.showticketingservice.model.user.UserSession;
-import org.junit.jupiter.api.*;
+import com.show.showticketingservice.model.user.UserUpdateRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.context.WebApplicationContext;
 
 import static com.show.showticketingservice.tool.constants.UserConstant.USER;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Transactional
 @AutoConfigureMockMvc
 @SpringBootTest
-public class MyPageControllerTest {
+class MyPageControllerTest {
 
-    private UserRequest userRequest;
+    private UserRequest testUser;
 
     private UserSession userSession;
 
-    private MockMvc mockMvc;
+    private UserUpdateRequest updateRequest;
 
     @Autowired
-    private WebApplicationContext wac;
+    MockMvc mvc;
 
     @Autowired
-    private ObjectMapper objectMapper;
+    ObjectMapper objectMapper;
+
+    @Autowired
+    MockHttpSession httpSession;
 
     @BeforeEach
-    public void setUp() throws Exception {
+    public void init() {
+        testUser = new UserRequest("testId1", "testPW1234#", "Test User", "010-1111-1111", "user1@example.com", "Seoul, South Korea", UserType.GENERAL);
 
-        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+        userSession = new UserSession(testUser.getUserId(), testUser.getUserType());
 
-        userRequest = userRequest.builder()
-                .userId("sin7416")
-                .password("123D!d4d4")
-                .name("미미")
-                .email("taa123@naver.com")
-                .phoneNum("010-1234-5678")
-                .address("경기도 부천시 오정구")
-                .userType(UserType.GENERAL)
-                .build();
+        updateRequest = new UserUpdateRequest("!validPW123", "010-1234-5678", "Busan, South Korea");
+    }
 
-        userSession = new UserSession("sin7416", UserType.GENERAL);
+    private void insertUser(UserRequest userRequest) throws Exception {
+        String content = objectMapper.writeValueAsString(userRequest);
 
-        String userContent = objectMapper.writeValueAsString(userRequest);
-
-        mockMvc.perform(post("/users")
-                .content(userContent)
+        mvc.perform(post("/users")
+                .content(content)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isCreated());
     }
 
-    @Test
-    @DisplayName("회원탈퇴시 status code 200을 리턴합니다.")
-    public void unregisterUser() throws Exception {
+    private void loginUser(UserRequest userRequest) throws Exception {
+        UserLoginRequest userLoginRequest = new UserLoginRequest(userRequest.getUserId(), userRequest.getPassword());
 
-        mockMvc.perform(post("/my-infos/unregister")
+        String content = objectMapper.writeValueAsString(userLoginRequest);
+
+        mvc.perform(post("/login")
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .session(httpSession))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("회원 탈퇴 시 status code 200을 리턴합니다.")
+    public void unregisterUser() throws Exception {
+        insertUser(testUser);
+        loginUser(testUser);
+
+        mvc.perform(post("/my-infos/unregister")
                 .sessionAttr(USER, userSession)
-                .param("passwordRequest", "123D!d4d4"))
+                .param("passwordRequest", testUser.getPassword()))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
 
     @Test
-    @DisplayName("회원탈퇴를 할 때 비밀번호가 일치하지 않을 시 status code 400을 리턴합니다.")
+    @DisplayName("회원 탈퇴 요청 중 비밀번호가 일치하지 않을 시 status code 400을 리턴합니다.")
     public void unregisterUserfail() throws Exception {
+        insertUser(testUser);
+        loginUser(testUser);
 
-        mockMvc.perform(post("/my-infos/unregister")
+        mvc.perform(post("/my-infos/unregister")
                 .sessionAttr(USER, userSession)
                 .param("passwordRequest", "124d4"))
                 .andDo(print())
                 .andExpect(status().isBadRequest());
     }
+
+    @Test
+    @DisplayName("회원 정보 수정 성공 시 Http Status 200 (OK) 리턴")
+    public void updateUserInfo() throws Exception {
+        insertUser(testUser);
+        loginUser(testUser);
+
+        String content = objectMapper.writeValueAsString(updateRequest);
+
+        mvc.perform(put("/my-infos")
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .session(httpSession))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("로그인 하지 않은 상태로 회원정보 수정 시도 시 Http Status 401 (Unauthorized) 리턴")
+    public void updateUserInfoWithoutLogin() throws Exception {
+        insertUser(testUser);
+
+        String content = objectMapper.writeValueAsString(updateRequest);
+
+        mvc.perform(put("/my-infos")
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .session(httpSession))
+                .andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @DisplayName("invalid 한 값(PW)으로 회원정보 수정 시도 시 Http Status 400 (Bad Request) 리턴")
+    public void updateUserWithInvalidPw() throws Exception {
+        insertUser(testUser);
+        loginUser(testUser);
+
+        UserUpdateRequest invalidRequest = UserUpdateRequest.builder()
+                .newPassword("invalidPw")
+                .newPhoneNum(updateRequest.getNewPhoneNum())
+                .newAddress(updateRequest.getNewAddress())
+                .build();
+
+        String content = objectMapper.writeValueAsString(invalidRequest);
+
+        mvc.perform(put("/my-infos")
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .session(httpSession))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("invalid 한 값(phoneNum)으로 회원정보 수정 시도 시 Http Status 400 (Bad Request) 리턴")
+    public void updateUserWithInvalidPhoneNum() throws Exception {
+        insertUser(testUser);
+        loginUser(testUser);
+
+        UserUpdateRequest invalidRequest = UserUpdateRequest.builder()
+                .newPassword(updateRequest.getNewPassword())
+                .newPhoneNum("010123-456")
+                .newAddress(updateRequest.getNewAddress())
+                .build();
+
+        String content = objectMapper.writeValueAsString(invalidRequest);
+
+        mvc.perform(put("/my-infos")
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .session(httpSession))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
 }
