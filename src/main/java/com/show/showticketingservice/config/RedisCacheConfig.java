cbaf965d6547af27cc472da894fb9c059b66e550
Jeonghan-Shin <alexsjh23@gmail.com>
2021-04-02 19:30:48 +0900

    [#45] RedisCacheConfig에 캐싱 관련 설명 주석으로 명시
    - CacheManager, RedisCacherManger, CacheResolver 설명

--- src/main/java/com/show/showticketingservice/config/RedisCacheConfig.java
+++ src/main/java/com/show/showticketingservice/config/RedisCacheConfig.java
@@ -31,6 +31,20 @@ public class RedisCacheConfig {
         return new LettuceConnectionFactory(new RedisStandaloneConfiguration(cacheHost, cachePort));
     }
 
+    /*
+        CacheManager:
+        - 스프링의 중앙 캐시 관리자 SPI(Service Provider Interface)로, 명명된 캐시 영역을 가져온다.
+
+        RedisCacheManager:
+        - Redis를 이용하여 캐싱을 적용할 땐 이 클래스로 CacheManager를 지원한다
+        - RedisCacheConfiguration의 기본 구성과 다른 구성이 필요한 캐시는
+          RedisCacheManager.RedisCacheManagerBuilder.withInitialCacheConfigurations(Map) 를 통해 지정 가능
+
+        CacheResolver:
+        - 캐시를 지원하는 resolver
+        - 인터셉트 된 메서드 호출에 사용할 캐시 인스턴스 결정
+        - CacheManager를 사용하여 탐색할 캐시 초기화
+     */
     @Bean
     public RedisCacheManager redisCacheManager(@Qualifier("redisCacheConnectionFactory") RedisConnectionFactory redisConnectionFactory) {
 
