f9808845172f79e3239fa0153c407247ed9993ec
Sin Hak <tls7gkr@naver.com>
2021-04-28 10:02:28 +0900

    [#85] 공연 상세 정보 이미지 조회
    - 같은 url에 공연 상세 정보와 공연 이미지를 같이 조회해야 돼서 공연 정보 상세 조회 handler에 공연 이미지 조회 추가
    - 프론트에서 응답받은 이미지 데이터를 이미지화 시켜 줌

--- src/main/java/com/show/showticketingservice/model/performance/PerformanceResponse.java
+++ src/main/java/com/show/showticketingservice/model/performance/PerformanceResponse.java
@@ -20,6 +20,8 @@ public class PerformanceResponse {
 
     private ShowType showType;
 
+    private String imageFilePath;
+
     private List<PerformanceTimeResponse> performanceTimeResponses;
 
     private VenueResponse venueResponse;
