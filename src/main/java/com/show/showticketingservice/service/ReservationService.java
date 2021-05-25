70490aafbbb37e50ca362068b2f4ed80484db66c
Jeonghan-Shin <alexsjh23@gmail.com>
2021-05-26 02:58:34 +0900

    [#77] 최대 예매 가능 좌석 수 지정
    - 소수의 회원이 많은 좌석을 독점하는 것을 방지하기 위해 단일 요청당 최대 예매 가능 좌석 수를 4개로 지정
    - 만약 허용 예매 좌석 수를 넘겨 예매 요청시 ReserveAllowedQuantityExceededException 예외 처리

--- src/main/java/com/show/showticketingservice/service/ReservationService.java
+++ src/main/java/com/show/showticketingservice/service/ReservationService.java
@@ -1,5 +1,6 @@
 package com.show.showticketingservice.service;
 
+import com.show.showticketingservice.exception.reservation.ReserveAllowedQuantityExceededException;
 import com.show.showticketingservice.mapper.ReservationMapper;
 import com.show.showticketingservice.model.reservation.ReservationRequest;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,8 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ReservationService {
 
+    private final static int MAX_RESERVATION_QUANTITY = 4;
+
     private final ReservationMapper reservationMapper;
 
     private final PerformanceService performanceService;
@@ -18,6 +21,9 @@ public class ReservationService {
 
     @Transactional
     public void reserveSeats(int userId, ReservationRequest reservationRequest) {
+        if (reservationRequest.getSeatIds().size() > MAX_RESERVATION_QUANTITY)
+            throw new ReserveAllowedQuantityExceededException("최대 예매 가능 좌석 수는 4자리 입니다.");
+
         performanceService.checkPerfTimeIdExists(reservationRequest.getPerfTimeId());
         seatService.setSeatsReserved(reservationRequest.getSeatIds());
         reservationMapper.reserveSeats(userId, reservationRequest.getSeatIds());
