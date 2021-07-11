d72f48664a41ae36a0b829c7dd0f2dc561a010ee
Jeonghan-Shin <alexsjh23@gmail.com>
2021-07-12 03:30:38 +0900

    공연 조회 캐싱 적용

--- src/main/java/com/show/showticketingservice/service/PerformanceService.java
+++ src/main/java/com/show/showticketingservice/service/PerformanceService.java
@@ -23,6 +23,8 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.IntStream;
 
+import static com.show.showticketingservice.tool.constants.CacheConstant.MAIN_PERFORMANCE_LIST_KEY;
+
 @Service
 @RequiredArgsConstructor
 public class PerformanceService {
@@ -287,28 +289,28 @@ public class PerformanceService {
       - ALL_TYPE_PERFORMANCE_LIST : 모든 타입의 공연을 선택했고 첫 번째 페이지를 제외한 경우
      */
     @Transactional
-//    @Caching(cacheable = {
-//            @Cacheable(
-//                    cacheNames = CacheConstant.MAIN_TYPED_PERFORMANCE_LIST,
-//                    condition = "#showType != null && #performancePagingCriteria.lastPerfId == null",
-//                    key = "#showType.toString()"
-//            ),
-//            @Cacheable(
-//                    cacheNames = CacheConstant.PAGED_TYPED_PERFORMANCE_LIST,
-//                    condition = "#showType != null && #performancePagingCriteria.lastPerfId != null",
-//                    key = "#showType.toString() + #performancePagingCriteria.lastPerfId"
-//            ),
-//            @Cacheable(
-//                    cacheNames = CacheConstant.MAIN_PERFORMANCE_LIST,
-//                    condition = "#showType == null && #performancePagingCriteria.lastPerfId == null",
-//                    key = MAIN_PERFORMANCE_LIST_KEY
-//            ),
-//            @Cacheable(
-//                    cacheNames = CacheConstant.PAGED_PERFORMANCE_LIST,
-//                    condition = "#showType == null && #performancePagingCriteria.lastPerfId != null",
-//                    key = "#performancePagingCriteria.lastPerfId"
-//            )
-//    })
+    @Caching(cacheable = {
+            @Cacheable(
+                    cacheNames = CacheConstant.MAIN_TYPED_PERFORMANCE_LIST,
+                    condition = "#showType != null && #performancePagingCriteria.lastPerfId == null",
+                    key = "#showType.toString()"
+            ),
+            @Cacheable(
+                    cacheNames = CacheConstant.PAGED_TYPED_PERFORMANCE_LIST,
+                    condition = "#showType != null && #performancePagingCriteria.lastPerfId != null",
+                    key = "#showType.toString() + #performancePagingCriteria.lastPerfId"
+            ),
+            @Cacheable(
+                    cacheNames = CacheConstant.MAIN_PERFORMANCE_LIST,
+                    condition = "#showType == null && #performancePagingCriteria.lastPerfId == null",
+                    key = MAIN_PERFORMANCE_LIST_KEY
+            ),
+            @Cacheable(
+                    cacheNames = CacheConstant.PAGED_PERFORMANCE_LIST,
+                    condition = "#showType == null && #performancePagingCriteria.lastPerfId != null",
+                    key = "#performancePagingCriteria.lastPerfId"
+            )
+    })
     public List<PerformanceResponse> getPerformances(ShowType showType, PerformancePagingCriteria performancePagingCriteria) {
         checkValidPerfIdAndShowType(showType, performancePagingCriteria.getLastPerfId());
         return performanceMapper.getPerformances(showType, performancePagingCriteria);
