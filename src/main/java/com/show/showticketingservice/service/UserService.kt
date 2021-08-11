14f5e4d731b28363dd7ddde0e92d919d3c25fa6c
Jeonghan-Shin <alexsjh23@gmail.com>
2021-08-11 20:02:21 +0900

    [#129] UserService java to kotlin 변환
    - 인텔리제이 자체 변환기 사용

---
+++ src/main/java/com/show/showticketingservice/service/UserService.kt
@@ -0,0 +1,66 @@
+package com.show.showticketingservice.service
+
+import com.show.showticketingservice.exception.authentication.UserIdAlreadyExistsException
+import com.show.showticketingservice.exception.authentication.UserIdNotExistsException
+import com.show.showticketingservice.exception.authentication.UserPasswordWrongException
+import com.show.showticketingservice.mapper.UserMapper
+import com.show.showticketingservice.model.user.UserRequest
+import com.show.showticketingservice.model.user.UserResponse
+import com.show.showticketingservice.model.user.UserSession
+import com.show.showticketingservice.model.user.UserUpdateRequest
+import com.show.showticketingservice.tool.encryptor.PasswordEncryptor
+import lombok.RequiredArgsConstructor
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@RequiredArgsConstructor
+class UserService {
+
+    private val userMapper: UserMapper? = null
+    private val passwordEncryptor: PasswordEncryptor? = null
+
+    @Transactional
+    fun signUp(userRequest: UserRequest) {
+        checkIdExists(userRequest.userId)
+        insertUser(userRequest)
+    }
+
+    fun insertUser(userRequest: UserRequest) {
+        userMapper!!.insertUser(userRequest.pwEncryptedUser(passwordEncryptor!!.encrypt(userRequest.password)))
+    }
+
+    @Transactional(readOnly = true)
+    fun checkIdExists(userId: String?) {
+        if (userMapper!!.isIdExists(userId)) {
+            throw UserIdAlreadyExistsException()
+        }
+    }
+
+    fun getUser(userIdRequest: String?, passwordRequest: String): UserResponse {
+        val userResponse = userMapper!!.getUserByUserId(userIdRequest) ?: throw UserIdNotExistsException()
+        if (!isPasswordMatches(passwordRequest, userResponse.password)) {
+            throw UserPasswordWrongException()
+        }
+        return userResponse
+    }
+
+    private fun isPasswordMatches(passwordRequest: String, userPassword: String): Boolean {
+        return passwordEncryptor!!.isMatched(passwordRequest, userPassword)
+    }
+
+    @Transactional
+    fun deleteUser(userSession: UserSession, passwordRequest: String) {
+        val hashPassword = userMapper!!.getUserPasswordById(userSession.userId)
+        if (!isPasswordMatches(passwordRequest, hashPassword)) {
+            throw UserPasswordWrongException()
+        }
+        userMapper.deleteUserById(userSession.userId)
+    }
+
+    @Transactional
+    fun updateUserInfo(userSession: UserSession, userUpdateRequest: UserUpdateRequest) {
+        val newEncryptedPassword = passwordEncryptor!!.encrypt(userUpdateRequest.newPassword)
+        userMapper!!.updateUserInfo(userSession.userId, userUpdateRequest.pwEncryptedUserUpdateRequest(newEncryptedPassword))
+    }
+}
